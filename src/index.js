b09121741027b3b34856f81cb8d73b348841806a
ahmetfurkankasikci <furkankasikci45@gmail.com>
2024-03-09 18:59:12 +0300

    sync

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Recipe from './pages/recipe';
 
-const router = createBrowserRouter([{
+const router = createHashRouter([{
   path: '/recipe',
   element: <Recipe />
 }])
