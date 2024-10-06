fa3c7e0070b56bee96bbb6f057975943dd12d256
ahmetfurkankasikci <furkankasikci45@gmail.com>
2024-10-06 22:51:42 +0300

    sync

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Recipe from "./pages/recipe/index";
 import ProductPreview from "./pages/product-preview-card";
+import NftCard from "./pages/nft_preview-card";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Recipe />} />
+        <Route path="/nft" element={<NftCard />} />      
         <Route path="/recipe" element={<Recipe />} />
         <Route path="/product" element={<ProductPreview />} />
+        <Route path="/" element={<Recipe />} />
       </Routes>
     </>
   );
---
+++ src/pages/nft_preview-card/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './style.module.css';
+function NftCard() {
+    return (
+        <div className={styles.body}>
+            <div className={styles.card}>
+                NftCard
+            </div>
+        </div>
+    )
+
+}
+
+export default NftCard
