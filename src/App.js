923c2f821ad4b8300e78bb21401bcccff8a32d19
ahmetfurkankasikci <furkankasikci45@gmail.com>
2024-11-04 22:55:12 +0300

    sync

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Recipe from "./pages/recipe/index";
 import ProductPreview from "./pages/product-preview-card";
 import ColumnPreview from "./pages/column-preview-card";
 import NftCard from "./pages/nft_preview-card";
+import BentoGrid from "./pages/bento-grid";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/nft" element={<NftCard />} />      
         <Route path="/recipe" element={<Recipe />} />
         <Route path="/product" element={<ProductPreview />} />
+        <Route path="/bento-grid" element={<BentoGrid />} />
         <Route path="/column" element={<ColumnPreview />} />
         <Route path="/" element={<Recipe />} />
       </Routes>
---
+++ src/pages/bento-grid/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './style.module.css';
+function BentoGrid() {
+  return (
+    <div>BentoGrid</div>
+  )
+}
+
+export default BentoGrid
